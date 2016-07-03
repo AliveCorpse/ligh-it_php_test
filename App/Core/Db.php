c0940fadc20c611393328845809964e5c5f8ffc4
Vladimir Kirejko <kirejko@gmail.com>
2016-07-03 15:10:15 +0300

    Db class

--- App/Core/Db.php
+++ App/Core/Db.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace App\Core;
+
+class Db
+{
+    use Singletone;
+
+    private $dbh;
+
+    protected function __construct()
+    {
+        try {
+            $this->dbh = new \PDO('mysql:host=localhost;dbname=lightit', 'root', '');
+        } catch (\PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    
+}
