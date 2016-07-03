0c7da5a120177cbce40f269ab0a7330003d82298
Vladimir Kirejko <kirejko@gmail.com>
2016-07-03 16:35:07 +0300

    View variables

--- App/Core/View.php
+++ App/Core/View.php
@@ -6,6 +6,21 @@ class View
 {
     protected $data = [];
 
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+
     public function render($template)
     {
         ob_start();
@@ -19,5 +34,10 @@ class View
         ob_clean();
 
         return $content;
+    }
+
+    public function display($template)
+    {
+        echo $this->render($template);
     }
 }
