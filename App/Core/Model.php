68076387d3496e1541a84599192d2c1e38b481ec
Vladimir Kirejko <kirejko@gmail.com>
2016-07-03 15:21:11 +0300

    Model-findAll()

---
+++ App/Core/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core;
+
+abstract class Model
+{
+    const TABLE = '';
+
+    public $id;
+
+    public static function findAll()
+    {
+        $db = Db::instance();
+        return $db->query(
+            'SELECT * FROM ' . static::TABLE,
+            [],
+            static::class
+        );
+    }
+}
